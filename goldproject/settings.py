42c036b681a1af809a7501f9e327de548513cd50
Aravindhakshan <aravindhakshan.k@venpep.com>
2025-04-30 14:59:04 +0530

    db setting commit

--- goldproject/settings.py
+++ goldproject/settings.py
@@ -80,17 +80,27 @@ WSGI_APPLICATION = 'goldproject.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'gold_app',
+#         'USER': 'postgres',
+#         'PASSWORD': '1234',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'gold_app',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',  
-        'PORT': '5432',      
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "12345678",
+        "HOST": "gls-db.cfm28okw4scw.eu-north-1.rds.amazonaws.com",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
